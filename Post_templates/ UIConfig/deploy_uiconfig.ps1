df61ba6954c6ac8af1ba16cf9e3aef2c8dbdd9ce
vicky-1626 <vigneshwaran.c.k@capgemini.com>
2024-11-28 19:06:59 +0530

    Create deploy_uiconfig.ps1

---
+++ Post_templates/ UIConfig/deploy_uiconfig.ps1
@@ -0,0 +1,119 @@
+param(
+[string]$REPOSITORY, [string]$ENVCONFIG_PATH
+)
+
+# if ($env:taskfail -eq 1 -Or $env:jobfail -eq "true")
+# {
+#   Write-Host "##vso[task.setvariable variable=agent.jobstatus;]Failed"
+#   Write-Host "##vso[task.complete result=Failed;]DONE"
+#   return
+# }
+# Write-Host "##vso[task.setvariable variable=taskfail]1"
+
+$script:pwd = (Get-Item -Path ".\")
+
+$Env_Config_Path = convertfrom-stringdata (get-content $pwd\$ENVCONFIG_PATH -raw)
+$TC_ROOT=$Env_Config_Path.TC_ROOT
+$TC_DATA=$Env_Config_Path.TC_DATA
+$UICONFIG_PATH=$Env_Config_Path.UICONFIG_PATH
+$UICONFIG_BAT=$Env_Config_Path.UICONFIG_BAT
+
+$ValidPath = Test-Path -Path $TC_ROOT
+if ($ValidPath -eq $False) 
+{
+    Write-Host "***ERROR:Failed to find the TC_ROOT path"
+    return
+}
+
+$ValidPath = Test-Path -Path $TC_DATA
+if ($ValidPath -eq $False) 
+{
+    Write-Host "***ERROR:Failed to find the TC_DATA path"
+    return
+}
+
+If($env:dryrun -eq "yes")
+{ 
+    Add-content $env:logdir/DryRunReport.txt "`n"
+}
+
+$filenames = Get-ChildItem -Path $pwd\$REPOSITORY\$UICONFIG_PATH -Recurse -Name -attributes !Directory | Where-Object {$_ -match "\.xml$"} 
+$error_level=0
+$siteName=(Get-Content $TC_DATA\model\siteinfo.properties | Where-Object {$_ -match "^siteName="} ).split("=")
+
+foreach ($filename in $filenames)
+{
+    
+    $only_filename = $filename.split("\")
+    
+    if ($only_filename)
+    {
+        $found = Get-Content $pwd\$REPOSITORY\MasterDelta.txt | Where-Object {$_ -match $only_filename} 
+        #$found = Get-Content "D:\apps_server\devops\Delta\MasterDelta.txt" | Where-Object {$_ -match $only_filename}
+        if ($found) 
+        {
+            Write-Host "Inside Found"
+            $match=$found
+            #$found = $found -replace "AWC/" , ""
+            $found = $found -replace "/$only_filename" , ""
+
+            Write-Host "Found value" $found "folder"
+            if ($found -eq "$UICONFIG_PATH" )
+            {
+                $command = "$(pwd)/$REPOSITORY/$UICONFIG_PATH/$UICONFIG_BAT `"$only_filename`" $TC_ROOT"
+                
+
+                If($env:dryrun -eq "yes")
+                {
+                    Add-content $env:logdir/DryRunReport.txt "Importing AWC UI_Config: $command"
+                }
+                else
+                {
+                    Write-Host  "Execute cmd="  $command
+                    $result = Invoke-Expression $command
+                    if($result.contains("Failed to import AWC UI_Config"))
+                    {
+                        Write-Host $result
+                        $error_level=1
+                    }
+                }
+            }
+            else
+            {
+                if (($siteName.Length -eq 2 )  -and ($siteName[1]))
+                {
+                    if($match.contains($siteName[1]))
+                    {
+                        $command = "$(pwd)/$REPOSITORY/$UICONFIG_PATH/$UICONFIG_BAT `"$only_filename[$only_filename.Length-1]`" $TC_ROOT"
+
+                        If($env:dryrun -eq "yes")
+                        {
+                            Add-content $env:logdir/DryRunReport.txt "Importing AWC UI_Config: $command"
+                        }
+                        else
+                        {
+                            Write-Host  "Execute cmd Site Sepecific ="  $command
+                            $result = Invoke-Expression $command
+                            
+                            if($result.contains("Failed to import AWC UI_Config"))
+                            {
+                                Write-Host $result
+                                $error_level=1
+                            }
+                        }
+                    }
+                }
+            }       
+        }
+    }
+}
+
+if($error_level -eq 0)
+{
+    Write-Host "##vso[task.setvariable variable=taskfail]0"
+}
+else
+{
+    Write-Host "Importing of AWC UI config failed"
+    exit 1
+}
