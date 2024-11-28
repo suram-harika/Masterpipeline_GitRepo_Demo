605917f2479862cfadbfaa9bc894f10a4018f7e5
vicky-1626 <vigneshwaran.c.k@capgemini.com>
2024-11-28 19:10:23 +0530

    Create deploy_closurerule.ps1

---
+++ Post_templates/ClosureRule/deploy_closurerule.ps1
@@ -0,0 +1,119 @@
+param(
+[string]$REPOSITORY, [string]$ENVCONFIG_PATH
+)
+
+# if ($env:taskfail -eq 1 -Or $env:jobfail -eq "true")
+# {
+#   Write-Host "##vso[task.setvariable variable=agent.jobstatus;]Failed"
+#   Write-Host "##vso[task.complete result=Failed;]DONE"
+#   return
+# }
+# Write-Host "##vso[task.setvariable variable=taskfail]1"
+
+$script:pwd = (Get-Item -Path ".\")
+
+$Env_Config_Path = convertfrom-stringdata (get-content $pwd\$ENVCONFIG_PATH -raw)
+$TC_ROOT=$Env_Config_Path.TC_ROOT
+$TC_DATA=$Env_Config_Path.TC_DATA
+$CLOSURERULE_PATH=$Env_Config_Path.CLOSURERULE_PATH
+$TRANSFERMODE_BAT=$Env_Config_Path.TRANSFERMODE_BAT
+
+$ValidPath = Test-Path -Path $TC_ROOT
+if ($ValidPath -eq $False) 
+{
+    Write-Host "***ERROR:Failed to find the TC_ROOT path"
+    return
+}
+
+$ValidPath = Test-Path -Path $TC_DATA
+if ($ValidPath -eq $False) 
+{
+    Write-Host "***ERROR:Failed to find the TC_DATA path"
+    return
+}
+
+If($env:dryrun -eq "yes")
+{ 
+    Add-content $env:logdir/DryRunReport.txt "`n"
+}
+
+$filenames = Get-ChildItem -Path $pwd\$REPOSITORY\$CLOSURERULE_PATH -Recurse -Name -attributes !Directory | Where-Object {$_ -match "\.xml$"} 
+
+$siteName=(Get-Content $TC_DATA\model\siteinfo.properties | Where-Object {$_ -match "^siteName="} ).split("=")
+$siteName = $siteName.split(" ")
+if($siteName.length -eq 2)
+{
+    $siteName  = $siteName[1]
+}
+$error_level=0
+foreach ($filename in $filenames)
+{
+    $filename = $filename.replace("\","/")
+    $full_name = $filename
+    if ($full_name)
+    {
+        $full_name = "$CLOSURERULE_PATH/$full_name"
+        $found = Get-Content $pwd\$REPOSITORY\MasterDelta.txt | Where-Object {$_ -match $full_name} 
+
+        $match=$found
+    
+        if ($found) 
+        {
+            $full_name_list = $full_name.split("/")
+            $only_filename =  $full_name_list[$full_name_list.length-1]
+            
+            $pathfolder = $found -replace "/$only_filename" , ""
+            
+            if ($pathfolder -eq "$CLOSURERULE_PATH")
+            {
+                $command = "$(pwd)/$REPOSITORY/$CLOSURERULE_PATH/$TRANSFERMODE_BAT  $filename $TC_ROOT"
+                If($env:dryrun -eq "yes")
+                {
+                    Add-content $env:logdir/DryRunReport.txt "Importing closurerule: $command"
+                }
+                else
+                {
+                    Write-Host  "Execute cmd= "  $command
+                    $result = Invoke-Expression $command
+                    if($result.contains("Failed to import closurerule"))
+                    {
+                        Write-Host $result
+                        $error_level=1
+                    }
+                }
+            }
+            else
+            {
+                if($match.contains($siteName))
+                {
+                    $command = "$(pwd)/$REPOSITORY/$CLOSURERULE_PATH/$TRANSFERMODE_BAT  $filename $TC_ROOT"
+                    If($env:dryrun -eq "yes")
+                    {
+                        Add-content $env:logdir/DryRunReport.txt "Importing closurerule: $command"
+                    }
+                    else
+                    {
+                        Write-Host  "Site Specific - Execute cmd= "  $command
+                        $result = Invoke-Expression $command
+                        if($result.contains("Failed to import closurerule"))
+                        {
+                            Write-Host $result
+                            $error_level=1
+                        }
+                    }
+                }
+            }       
+        }
+    }
+}
+
+if($error_level -eq 0)
+{
+    Write-Host "##vso[task.setvariable variable=taskfail]0"
+}
+else
+{
+    Write-Host "Closurerule Import failed"
+    exit 1
+}
+
