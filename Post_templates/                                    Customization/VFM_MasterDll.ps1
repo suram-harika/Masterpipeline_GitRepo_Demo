3daade58b64de18adcfbf61a3ff96088ff7b039f
vicky-1626 <vigneshwaran.c.k@capgemini.com>
2024-12-02 17:04:28 +0530

    Create VFM_MasterDll.ps1

---
+++ Post_templates/                                    Customization/VFM_MasterDll.ps1
@@ -0,0 +1,13 @@
+param(
+    [string]$VFMDllMapping, [string]$ExeSharedLocation
+)
+
+Foreach ($row in (Get-Content -Path $VFMDllMapping | Where {$_ -notmatch '^#.*'}|Select-String -Pattern (Get-ChildItem -Path $ExeSharedLocation| %{$_.Name}))) 
+{ 
+  $DllName=$row.Line.Split('|')[0]
+  $Destination=$row.Line.Split('|')[1]  
+   if (!(Test-Path -path $Destination)) {New-Item $Destination -Type Directory}
+   Copy-Item -Path $ExeSharedLocation\$ExeName $Destination -Force
+   Write-Host "Copying $ExeName to the $Destination"
+}
+
