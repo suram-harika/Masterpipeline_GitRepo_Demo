1d4304c3339f064fdb184d15bda2cf6f7d194774
vicky-1626 <vigneshwaran.c.k@capgemini.com>
2024-12-02 17:05:55 +0530

    Create VFM_ModuleExe.ps1

---
+++ Post_templates/                                    Customization/VFM_ModuleExe.ps1
@@ -0,0 +1,13 @@
+param(
+    [string]$VFM_ModuleExeMapping, [string]$ExeSharedLocation
+)
+
+Foreach ($row in (Get-Content -Path $VFM_ModuleExeMapping | Where {$_ -notmatch '^#.*'}|Select-String -Pattern (Get-ChildItem -Path $ExeSharedLocation| %{$_.Name}))) 
+{ 
+  $ExeName=$row.Line.Split('|')[0]
+  $Destination=$row.Line.Split('|')[1]  
+   if (!(Test-Path -path $Destination)) {New-Item $Destination -Type Directory}
+   Copy-Item -Path $ExeSharedLocation\$ExeName $Destination -Force
+   Write-Host "Copying $ExeName to the $Destination"
+}
+ 
