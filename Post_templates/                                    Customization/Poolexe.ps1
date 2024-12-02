30723c5bfa50ccab72faadd29da4c18868be6bf3
vicky-1626 <vigneshwaran.c.k@capgemini.com>
2024-12-02 16:57:41 +0530

    Create Poolexe.ps1

---
+++ Post_templates/                                    Customization/Poolexe.ps1
@@ -0,0 +1,13 @@
+param(
+    [string]$POOLExeMapping, [string]$ExeSharedLocation
+)
+
+Foreach ($row in (Get-Content -Path $POOLExeMapping | Where {$_ -notmatch '^#.*'}|Select-String -Pattern (Get-ChildItem -Path $ExeSharedLocation| %{$_.Name}))) 
+{ 
+  $ExeName=$row.Line.Split('|')[0]
+  $Destination=$row.Line.Split('|')[1]  
+   if (!(Test-Path -path $Destination)) {New-Item $Destination -Type Directory}
+   Copy-Item -Path $ExeSharedLocation\$ExeName $Destination -Force
+   Write-Host "Copying $ExeName to the $Destination"
+}
+ 
