ee0a4e9091b403811a75fcec2fd3c0dbe0334f75
vicky-1626 <vigneshwaran.c.k@capgemini.com>
2024-12-09 18:51:20 +0530

    Update import_revisionrule.bat

--- 4 - Deploy_Scripts/Post-bmide/RevisionRule/import_revisionrule.bat
+++ 4 - Deploy_Scripts/Post-bmide/RevisionRule/import_revisionrule.bat
@@ -8,8 +8,6 @@ rem
 IF %1 == goto invalidrevfile_nameerror
  set TC_ROOT=%2
 if not defined TC_ROOT  goto tcrooterror
-set CAT_SSO=NO
-call %TC_ROOT%\cat\cat_setenv.bat
 
 if not defined TC_BIN   goto tcbinerror
 if not defined TC_DATA  goto tcdataerror
