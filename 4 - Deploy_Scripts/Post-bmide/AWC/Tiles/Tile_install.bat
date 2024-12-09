d5ad8ecfea0c35ccf1deb2c56dbae1a5bb9177ae
vicky-1626 <vigneshwaran.c.k@capgemini.com>
2024-12-09 18:21:03 +0530

    Update Tile_install.bat

--- 4 - Deploy_Scripts/Post-bmide/AWC/Tiles/Tile_install.bat
+++ 4 - Deploy_Scripts/Post-bmide/AWC/Tiles/Tile_install.bat
@@ -4,18 +4,12 @@ REM set tc_root=X:\Siemens\Teamcenter11
 REM set tc_data=X:\Siemens\tcdata11
 REM call %tc_data%\tc_profilevars.bat
 
-set cat_sso=NO
-
-call D:\apps_server\Siemens\cat\cat_setenv.bat
-rem call X:\Siemens\Teamcenter11\cat\cat_setenv.bat
 set file = %1
 
 
 REM D:\apps_server\Siemens\Teamcenter14\bin\aws2_install_tilecollections -u=infodba -pf=%ipwfile%  -g=dba -mode=remove -file="%~dp0\AWC_iPi_Tiles_Config_remove.xml"
 
 REM D:\apps_server\Siemens\Teamcenter14\bin\aws2_install_tilecollections -u=infodba -pf=%ipwfile%  -g=dba -mode=add -file="%~dp0\AWC_iPi_Tiles_Config_Update.xml"
-
-call D:\apps_server\Siemens\Teamcenter14\bin\aws2_install_tilecollections -u=infodba -pf=%ipwfile%  -g=dba -mode=update -file="%~dp0\CATUpdateRequestTile.xml"
 
 REM call D:\apps_server\Siemens\Teamcenter14\bin\aws2_install_tilecollections -u=infodba -pf=%ipwfile%  -g=dba -mode=add -file="%~dp0\EnableNowLearningTemplate.xml" 
 
