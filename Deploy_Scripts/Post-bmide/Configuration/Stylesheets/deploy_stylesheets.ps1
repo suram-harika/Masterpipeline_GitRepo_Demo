a56be0038c3294d16a3782dfc7d5d72b40454fff
Venkata Reddy, Maredla <maredla.reddy@capgemini.com>
2025-03-07 16:10:03 +0000

    Updated deploy_stylesheets.ps1

--- Deploy_Scripts/Post-bmide/Configuration/Stylesheets/deploy_stylesheets.ps1
+++ Deploy_Scripts/Post-bmide/Configuration/Stylesheets/deploy_stylesheets.ps1
@@ -1,7 +1,7 @@
 param(
 [string]$REPOSITORY, [string]$ENVCONFIG_PATH
 )
-
+ 
 # if ($env:taskfail -eq 1 -Or $env:jobfail -eq "true")
 # {
 #   Write-Host "##vso[task.setvariable variable=agent.jobstatus;]Failed"
@@ -11,10 +11,10 @@ param(
 # Write-Host "##vso[task.setvariable variable=taskfail]1"
 $FOLDER_PATH='Stylesheets'
 $script:pwd = (Get-Item -Path ".\")
-
+ 
 #Fetches the Env_Config.txt file which is in Deploy_Scripts repository.Assigns value to the below variables
 $Env_Config_Path = convertfrom-stringdata (get-content $pwd\$ENVCONFIG_PATH -raw)
-
+ 
 $TC_ROOT=$Env_Config_Path.TC_ROOT
 $TC_DATA=$Env_Config_Path.TC_DATA
 $STYLESHEET_PATH=$Env_Config_Path.STYLESHEET_PATH
@@ -22,9 +22,9 @@ $STYLESHEET_PATH=$STYLESHEET_PATH.Replace("/", "\")
 #$STYLESHEET_PATH=$Env_Config_Path.RAC_PATH
 #$AWC_PATH=$Env_Config_Path.AWC_PATH
 $STYLESHEET_BAT=$Env_Config_Path.STYLESHEET_BAT
-
+ 
 $cat_setenv=$Env_Config_Path.cat_setenv
-
+ 
 #Tests TC_ROOT a valid path or not
 $ValidPath = Test-Path -Path $TC_ROOT
 if ($ValidPath -eq $False) 
@@ -40,19 +40,20 @@ if ($ValidPath -eq $False)
     Write-Host "***ERROR:Failed to find the TC_DATA path"
     return
 }
-
-
+ 
+ 
+ 
 #Gets all the xmls of RAC from the Rich_client/Stylesheets path
     $filenames = Get-ChildItem -Path $pwd\$REPOSITORY\$STYLESHEET_PATH\ -Name -attributes !Directory | Where-Object {$_ -match "\.xml$"} 
     $error_value=0
     Write-Host "1"
         #Compares the filenames(i.e, xml from RAC stylesheets folder with masterdelta.txt)
         $found = Get-Content $pwd\$REPOSITORY\MasterDelta.txt 
-
+ 
 foreach ($file in $found) { 
-
+ 
 $FileName = [System.IO.Path]::GetFileName($file) 
-
+ 
 if ($filenames -contains $FileName) { 
     Write-Host $FileName 
     $file_withoutextension = [System.IO.Path]::GetFileNameWithoutExtension($FileName)
@@ -60,7 +61,7 @@ if ($filenames -contains $FileName) {
         Add-Content -Path $pwd\$REPOSITORY\$STYLESHEET_PATH\input_stylesheets.txt -Value $line
                 
     } }
-
+ 
     If($env:dryrun -eq "yes")
     { 
         Add-content $env:logdir/DryRunReport.txt "`n"
@@ -88,9 +89,10 @@ if ($filenames -contains $FileName) {
             
         }
     }
-
+ 
+ 
 }
-
+ 
 if ($error_value -eq 0)
 {
     Write-Host "##vso[task.setvariable variable=taskfail]0"
@@ -99,4 +101,4 @@ else
 {
    Write-Host "Style Sheet import operation failed"
    exit 1
-} 
+}
