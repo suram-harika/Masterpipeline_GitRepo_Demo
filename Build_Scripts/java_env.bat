9f49ec2ca294e9c88dd41278e778bdf8eaa2f6d3
Suram, Venkata Subbu Harika <harika.suram@capgemini.com>
2026-02-04 00:27:02 +0000

    Updated java_env.bat

--- Build_Scripts/java_env.bat
+++ Build_Scripts/java_env.bat
@@ -5,4 +5,4 @@ rem set JRE_HOME=C:\apps\siemens\Java64\jdk-11\jre
 set CLASSPATH=%TC_ROOT%\portal
 set PATH=%FMS_HOME%\bin;%FMS_HOME%\lib;TC_ROOT\portal;%PATH%;C:\apache-ant-1.10.13\bin
 rem start "TAO ImR" /min cmd /c "TC_ROOT\iiopservers\start_imr.bat"
-set ANT_HOME=C:\apache-ant-1.10.13
+set ANT_HOME=C:\apache-ant-1.10.14
