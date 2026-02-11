5aa3789378f26cc9ffa5ff4c31a39687d3983919
Suram, Venkata Subbu Harika <harika.suram@capgemini.com>
2026-02-11 14:16:11 +0000

    Updated java_env.bat

--- Build_Scripts/java_env.bat
+++ Build_Scripts/java_env.bat
@@ -1,4 +1,4 @@
-set TC_ROOT=C:\apps\siemens\Teamcenter2412
+set TC_ROOT=C:\apps\siemens\Teamcenter14
 set FMS_HOME=C:\apps\siemens\Teamcenter14\tccs
 set JAVA_HOME=C:\apps\siemens\Java64\jdk-11.0.15.10-hotspot
 rem set JRE_HOME=C:\apps\siemens\Java64\jdk-11\jre
